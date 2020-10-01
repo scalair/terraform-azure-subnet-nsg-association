94628d9a1f79b98de4c2dc962bf8f305665f1c0f
Xat59 <descamps.jeremy@gmail.com>
2020-10-01 13:09:30 +0200

    Module that allows association of a NSG with a subnet

---
+++ output.tf
@@ -0,0 +1,3 @@
+output subnet_id {
+  value = azurerm_subnet_network_security_group_association.nsg_association.id
+}
---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "subnet_id" {
+  description = "(Required) The ID of the Subnet. Changing this forces a new resource to be created."
+  type = string
+}
+
+variable "network_security_group_id" {
+  description = "(Required) The ID of the Network Security Group which should be associated with the Subnet. Changing this forces a new resource to be created."
+  type = string
+}
